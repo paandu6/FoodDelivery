e887204e1775c4883cfee497e4db310ac152fa65
paandu6 <panduale809@gmail.com>
2022-08-18 21:05:05 +0530

    added meals summary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Layout/Header";
+import MealsSummary from "./Components/Meals/MealsSummary";
 
 function App() {
   return (
     <div>
       <Header />
+      <MealsSummary />
     </div>
   );
 }
