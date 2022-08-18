0e15bdaf7a8e541976f34497140cbc9c14d071f3
paandu6 <panduale809@gmail.com>
2022-08-18 20:59:06 +0530

    created header

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import classes from './Header.module.css'
+import meanlsImage from '../../Assets/meals.jpg'
+import HeaderCartButton from "./HeaderCartButton";
+function Header(){
+    return(
+        <Fragment>
+            <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={meanlsImage}/>
+            </div>
+        </Fragment>
+    )
+}
+export default Header
